7362af86772a059c8036b54b753de4b4c83feb0b
Ha <thuha241706@gmail.com>
2026-09-15 23:02:19 +0300

    part 11-03 best exam result

--- part11-03_best_exam_result/src/best_exam_result.py
+++ part11-03_best_exam_result/src/best_exam_result.py
@@ -9,3 +9,13 @@ class ExamResult:
     def __str__(self):
         return (f'Name:{self.name}, grade1: {self.grade1}' +
             f', grade2: {self.grade2}, grade3: {self.grade3}')
+
+def best_results(results: list):
+    return [max(result.grade1, result.grade2, result.grade3) for result in results]
+
+if __name__ == "__main__":
+    result1 = ExamResult("Peter",5,3,4)
+    result2 = ExamResult("Pippa",3,4,1)
+    result3 = ExamResult("Paul",2,1,3)
+    results = [result1, result2, result3]
+    print(best_results(results))
